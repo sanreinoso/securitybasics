f48e59b11c5f45af943b3aaf557b7ab40721b957
Santiago Reinoso <44629364+sanreinoso@users.noreply.github.com>
2024-09-21 12:53:56 +0200

    Refactor project structure

---
+++ section2/src/main/java/com/sanreinoso/securityspring/SecurityBasicsApplication.java
@@ -0,0 +1,14 @@
+package com.sanreinoso.securityspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SecurityBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityBasicsApplication.class, args);
+	}
+
+}
---
+++ section3/src/main/java/com/sanreinoso/securityspring/config/ProjectSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.sanreinoso.securityspring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/myAccount", "/myBalance", "/myCards", "myLoans").authenticated()
+                .requestMatchers("/contact", "/notices", "/error").permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.withUsername("santy").password("{noop}12345").roles("read").build();
+        //admin password: admin
+        UserDetails admin = User.withUsername("admin")
+                .password("{bcrypt}$2a$12$LatfFicZHeCZQrbZAkw3U.vOzEy1xsgPkNASe6iSyXyGgCRRdpaJO").roles("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
