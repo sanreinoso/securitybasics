5d3e4c5454438c1224fb35a09572d4b4fa0d9ef5
Santiago Reinoso <44629364+sanreinoso@users.noreply.github.com>
2024-10-06 12:09:51 +0200

    New Section (section6) finished
    - Proveedor de autenticación propio
    - Configurar la aplicación para que funcione con diferentes perfiles

---
+++ section6/src/main/java/com/sanreinoso/securityspring/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.sanreinoso.securityspring.repository;
+
+import com.sanreinoso.securityspring.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    Optional<Customer> findByEmail(String email);
+}
