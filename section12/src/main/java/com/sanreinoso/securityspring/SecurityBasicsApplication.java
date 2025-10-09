1a0b09ab6a44d0a3bf0762381cfcb9b74939efa6
Santiago Reinoso <sanreinoso96@hotmail.com>
2025-10-09 18:04:58 +0200

    PostFilter and PreFilter in contacts

--- section12/src/main/java/com/sanreinoso/securityspring/SecurityBasicsApplication.java
+++ section12/src/main/java/com/sanreinoso/securityspring/SecurityBasicsApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity(debug = false)
+@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
 public class SecurityBasicsApplication {
 
 	public static void main(String[] args) {
