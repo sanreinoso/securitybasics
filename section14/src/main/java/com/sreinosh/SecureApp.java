d1d59dbfda638041ab68e68e580787241d5ad294
Santiago Reinoso <sanreinoso96@hotmail.com>
2025-11-04 18:40:47 +0100

    OAuth using social media
    - GitHub
    - Facebook

---
+++ section14/src/main/java/com/sreinosh/SecureApp.java
@@ -0,0 +1,13 @@
+package com.sreinosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureApp.class, args);
+	}
+
+}
